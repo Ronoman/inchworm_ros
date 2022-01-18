5b851590246dc4b7d9382c51c9aad73e8d10bbc7
Eli Benevedes <benevedeselias@gmail.com>
2022-01-18 18:15:59 -0500

    Initial inchworm hardware interface

---
+++ inchworm_hw_interface/src/inchworm_hw_interface.cpp
@@ -0,0 +1,224 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, PickNik LLC
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik LLC nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Dave Coleman
+   Desc:   Helper ros_control hardware interface that loads configurations
+*/
+
+#include "../include/inchworm_hw_interface.h"
+#include <limits>
+
+// ROS parameter loading
+#include <rosparam_shortcuts/rosparam_shortcuts.h>
+
+namespace inchworm_hw_interface
+{
+InchwormHWInterface::InchwormHWInterface(const ros::NodeHandle& nh)
+  : name_("inchworm_hw_interface"), nh_(nh), use_rosparam_joint_limits_(false)
+{
+  // Load rosparams
+  ros::NodeHandle rpnh(
+      nh_, "hardware_interface");  // TODO(davetcoleman): change the namespace to "inchworm_hw_interface" aka name_
+  std::size_t error = 0;
+  error += !rosparam_shortcuts::get(name_, rpnh, "joints", joint_names_);
+  rosparam_shortcuts::shutdownIfError(name_, error);
+}
+
+void InchwormHWInterface::init()
+{
+  num_joints_ = joint_names_.size();
+
+  // Status
+  joint_position_.resize(num_joints_, 0.0);
+  joint_velocity_.resize(num_joints_, 0.0);
+  joint_effort_.resize(num_joints_, 0.0);
+
+  // Command
+  joint_position_command_.resize(num_joints_, 0.0);
+  joint_velocity_command_.resize(num_joints_, 0.0);
+  joint_effort_command_.resize(num_joints_, 0.0);
+
+  // Limits
+  joint_position_lower_limits_.resize(num_joints_, 0.0);
+  joint_position_upper_limits_.resize(num_joints_, 0.0);
+  joint_velocity_limits_.resize(num_joints_, 0.0);
+  joint_effort_limits_.resize(num_joints_, 0.0);
+
+  // Initialize interfaces for each joint
+  for (std::size_t joint_id = 0; joint_id < num_joints_; ++joint_id)
+  {
+    ROS_DEBUG_STREAM_NAMED(name_, "Loading joint name: " << joint_names_[joint_id]);
+
+    // Create joint state interface
+    joint_state_interface_.registerHandle(hardware_interface::JointStateHandle(
+        joint_names_[joint_id], &joint_position_[joint_id], &joint_velocity_[joint_id], &joint_effort_[joint_id]));
+
+    // Add command interfaces to joints
+    // TODO: decide based on transmissions?
+    hardware_interface::JointHandle joint_handle_position = hardware_interface::JointHandle(
+        joint_state_interface_.getHandle(joint_names_[joint_id]), &joint_position_command_[joint_id]);
+    position_joint_interface_.registerHandle(joint_handle_position);
+
+    hardware_interface::JointHandle joint_handle_velocity = hardware_interface::JointHandle(
+        joint_state_interface_.getHandle(joint_names_[joint_id]), &joint_velocity_command_[joint_id]);
+    velocity_joint_interface_.registerHandle(joint_handle_velocity);
+
+    hardware_interface::JointHandle joint_handle_effort = hardware_interface::JointHandle(
+        joint_state_interface_.getHandle(joint_names_[joint_id]), &joint_effort_command_[joint_id]);
+    effort_joint_interface_.registerHandle(joint_handle_effort);
+
+    // Load the joint limits
+    registerJointLimits(joint_handle_position, joint_handle_velocity, joint_handle_effort, joint_id);
+  }  // end for each joint
+
+  registerInterface(&joint_state_interface_);     // From RobotHW base class.
+  registerInterface(&position_joint_interface_);  // From RobotHW base class.
+  registerInterface(&velocity_joint_interface_);  // From RobotHW base class.
+  registerInterface(&effort_joint_interface_);    // From RobotHW base class.
+
+  ROS_INFO_STREAM_NAMED(name_, "InchwormHWInterface Ready.");
+}
+
+void InchwormHWInterface::registerJointLimits(const hardware_interface::JointHandle& joint_handle_position,
+                                             const hardware_interface::JointHandle& joint_handle_velocity,
+                                             const hardware_interface::JointHandle& joint_handle_effort,
+                                             std::size_t joint_id)
+{
+  // Default values
+  joint_position_lower_limits_[joint_id] = -std::numeric_limits<double>::max();
+  joint_position_upper_limits_[joint_id] = std::numeric_limits<double>::max();
+  joint_velocity_limits_[joint_id] = std::numeric_limits<double>::max();
+  joint_effort_limits_[joint_id] = std::numeric_limits<double>::max();
+
+  // Limits datastructures
+  joint_limits_interface::JointLimits joint_limits;     // Position
+  joint_limits_interface::SoftJointLimits soft_limits;  // Soft Position
+  bool has_joint_limits = false;
+
+  // Get limits from ROS param
+  if (use_rosparam_joint_limits_)
+  {
+    if (joint_limits_interface::getJointLimits(joint_names_[joint_id], nh_, joint_limits))
+    {
+      has_joint_limits = true;
+      ROS_DEBUG_STREAM_NAMED(name_, "Joint " << joint_names_[joint_id] << " has rosparam position limits ["
+                                             << joint_limits.min_position << ", " << joint_limits.max_position << "]");
+      if (joint_limits.has_velocity_limits)
+        ROS_DEBUG_STREAM_NAMED(name_, "Joint " << joint_names_[joint_id] << " has rosparam velocity limit ["
+                                               << joint_limits.max_velocity << "]");
+    }  // the else debug message provided internally by joint_limits_interface
+  }
+
+  // Quit we we haven't found any limits in URDF or rosparam server
+  if (!has_joint_limits)
+  {
+    return;
+  }
+
+  // Copy position limits if available
+  if (joint_limits.has_position_limits)
+  {
+    // Slighly reduce the joint limits to prevent floating point errors
+    joint_limits.min_position += std::numeric_limits<double>::epsilon();
+    joint_limits.max_position -= std::numeric_limits<double>::epsilon();
+
+    joint_position_lower_limits_[joint_id] = joint_limits.min_position;
+    joint_position_upper_limits_[joint_id] = joint_limits.max_position;
+  }
+
+  // Copy velocity limits if available
+  if (joint_limits.has_velocity_limits)
+  {
+    joint_velocity_limits_[joint_id] = joint_limits.max_velocity;
+  }
+
+  // Copy effort limits if available
+  if (joint_limits.has_effort_limits)
+  {
+    joint_effort_limits_[joint_id] = joint_limits.max_effort;
+  }
+
+  ROS_DEBUG_STREAM_NAMED(name_, "Using saturation limits (not soft limits)");
+  
+  const joint_limits_interface::PositionJointSaturationHandle sat_handle_position(joint_handle_position, joint_limits);
+  pos_jnt_sat_interface_.registerHandle(sat_handle_position);
+  
+  const joint_limits_interface::VelocityJointSaturationHandle sat_handle_velocity(joint_handle_velocity, joint_limits);
+  vel_jnt_sat_interface_.registerHandle(sat_handle_velocity);
+  
+  const joint_limits_interface::EffortJointSaturationHandle sat_handle_effort(joint_handle_effort, joint_limits);
+  eff_jnt_sat_interface_.registerHandle(sat_handle_effort);
+}
+
+void InchwormHWInterface::reset()
+{
+  // Reset joint limits state, in case of mode switch or e-stop
+  pos_jnt_sat_interface_.reset();
+}
+
+void InchwormHWInterface::printState()
+{
+  // WARNING: THIS IS NOT REALTIME SAFE
+  // FOR DEBUGGING ONLY, USE AT YOUR OWN ROBOT's RISK!
+  ROS_INFO_STREAM_THROTTLE(1, std::endl << printStateHelper());
+}
+
+std::string InchwormHWInterface::printStateHelper()
+{
+  std::stringstream ss;
+  std::cout.precision(15);
+
+  for (std::size_t i = 0; i < num_joints_; ++i)
+  {
+    ss << "j" << i << ": " << std::fixed << joint_position_[i] << "\t ";
+    ss << std::fixed << joint_velocity_[i] << "\t ";
+    ss << std::fixed << joint_effort_[i] << std::endl;
+  }
+  return ss.str();
+}
+
+std::string InchwormHWInterface::printCommandHelper()
+{
+  std::stringstream ss;
+  std::cout.precision(15);
+  ss << "    position     velocity         effort  \n";
+  for (std::size_t i = 0; i < num_joints_; ++i)
+  {
+    ss << "j" << i << ": " << std::fixed << joint_position_command_[i] << "\t ";
+    ss << std::fixed << joint_velocity_command_[i] << "\t ";
+    ss << std::fixed << joint_effort_command_[i] << std::endl;
+  }
+  return ss.str();
+}
+}  // namespace inchworm_hw_interface
