3fa8d3e1a02c1fb75656c1624f35bf1d69d38cd5
Sam <ssgould25@gmail.com>
2022-02-18 14:20:03 -0500

    basic_control.py now has quintic control

--- inchworm_control/scripts/basic_control.py
+++ inchworm_control/scripts/basic_control.py
@@ -1,36 +1,89 @@
 #!/usr/bin/env python3
 
-import rospy, math
+from cmath import pi
+import rospy, math, sys, tf2_ros
+
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+from sensor_msgs.msg import JointState
+
+from traj_planner import TrajectoryPlanner
+
+trajectory_pub   = None
+mag_state_pub    = None
+last_joint_state = None
+
+def jointStateCB(msg):
+    global last_joint_state
+
+    last_joint_state = msg
+
+
+### TODO: This block should probably move to a library
+
+def joint(joint_angles, duration):
+  last_states = last_joint_state
+
+  joint_names = ["iw_ankle_foot_bottom", "iw_beam_ankle_bottom", "iw_mid_joint", "iw_beam_ankle_top", "iw_ankle_foot_top"]
+  cur_angles = []
+
+  # Reorder the joint names to be the order specified by joint_names
+  for name in joint_names:
+    cur_angles.append(last_states.position[last_states.name.index(name)])
+
+  # The desired angles from the payload
+  new_angles = [float(q) for q in joint_angles]
+
+  NUM_PTS = 50
+
+  # Calculate the quintic trajectory for each joint. Impose 0 velocity and 0 acceleration at limits.
+  traj_triplets = []
+  for (p0, pf) in zip(cur_angles, new_angles):
+    traj = TrajectoryPlanner.quintic_interp(duration, p0, pf, 0, 0, 0, 0, NUM_PTS)
+    traj_triplets.append(traj)
+
+  traj_pts = []
+
+  # 
+  for i in range(NUM_PTS):
+    pt = JointTrajectoryPoint()
+    pt.positions     = [traj_triplets[joint][0][i] for joint in range(5)]
+    pt.velocities    = [traj_triplets[joint][1][i] for joint in range(5)]
+    pt.accelerations = [traj_triplets[joint][2][i] for joint in range(5)]
+
+    pt.time_from_start = rospy.Duration(i * (duration / NUM_PTS))
+
+    traj_pts.append(pt)
+
+  trajectory = JointTrajectory()
+  trajectory.joint_names = joint_names
+  trajectory.points = traj_pts
+
+  trajectory.header.stamp = rospy.Time.now()
+  trajectory.header.frame_id = "world"
+
+  trajectory_pub.publish(trajectory)
+
+  rospy.sleep(duration)
+
 
 if __name__ == "__main__":
     rospy.init_node("basic_control")
 
-    pub = rospy.Publisher("/inchworm/position_trajectory_controller/command", JointTrajectory, queue_size=1)
+    rospy.Subscriber("/inchworm/joint_states", JointState, jointStateCB)
+    trajectory_pub = rospy.Publisher("/inchworm/position_trajectory_controller/command", JointTrajectory, queue_size=1)
 
     while True:
-        # We need to create a JointTrajectory to pass to the position_trajectory_controller
-        msg = JointTrajectory()
-        msg.header.frame_id = "world"
-
-        # These are the five joints on the robot.
-        msg.joint_names = ["iw_ankle_foot_bottom", "iw_beam_ankle_bottom", "iw_mid_joint", "iw_beam_ankle_top", "iw_ankle_foot_top"]
-
-        # To 
-        msg.points.append(JointTrajectoryPoint())
-
-        msg.points[0].positions = [0] * 5
-        msg.points[0].velocities = [0] * 5
-        msg.points[0].accelerations = [0] * 5
-
-        msg.points[0].time_from_start = rospy.Duration(1.0)
+        
+        goals = [0] * 5
 
         for i in range(5):
 
-            pos = float(input(f"Joint {i} position (rad): "))
-            msg.points[0].positions[i] = (pos)
+            pos = float(input(f"Joint {i} position: "))
+            if (abs(pos) > 2.2): # Eli can have his radians, I can have degrees
+                pos = pos * pi / 180
+            goals[i] = (pos)
 
-        msg.header.stamp = rospy.Time.now()
-
-        pub.publish(msg)
+        joint(goals, (5.0))
