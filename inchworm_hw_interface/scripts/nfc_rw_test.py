b235eb4e8c798b4007eb928e2c71b4470392730f
Eli Benevedes <benevedeselias@gmail.com>
2022-03-23 13:01:06 -0400

    Better RW test

--- inchworm_hw_interface/scripts/nfc_rw_test.py
+++ inchworm_hw_interface/scripts/nfc_rw_test.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-import rospy, random
+import rospy, random, time
 
 from inchworm_hw_interface.srv import ReadNFCBlock, ReadNFCBlockRequest, WriteNFCBlock, WriteNFCBlockRequest
 
 def main():
   rospy.init_node("nfc_rw_test")
 
+  print("Waiting for services...")
+
+  rospy.wait_for_service("/inchworm/nfc_write")
+  rospy.wait_for_service("/inchworm/nfc_read")
+
   write_proxy = rospy.ServiceProxy("/inchworm/nfc_write", WriteNFCBlock)
   read_proxy = rospy.ServiceProxy("/inchworm/nfc_read", ReadNFCBlock)
 
   TEST_DATA = [random.randint(0, 255) for i in range(16)]
+
+  print("Test data:")
+  print(TEST_DATA)
 
   write_req = WriteNFCBlockRequest()
   write_req.bottom_foot = True
@@ -19,6 +27,9 @@ def main():
 
   res = write_proxy(write_req)
 
+  print("Made write request.")
+  time.sleep(1)
+
   read_req = ReadNFCBlockRequest()
 
   read_req.bottom_foot = True
@@ -26,7 +37,9 @@ def main():
 
   res = read_proxy(read_req)
 
-  block_data = res.data
+  block_data = [int(elem) for elem in res.data]
+
+  print(block_data)
 
   if(block_data != TEST_DATA):
     print("Read did not match write :(")
