15b50398cb31a1c972fdd9a30734adb5b25a2215
Sam <ssgould25@gmail.com>
2022-04-16 13:38:25 -0400

    1 inchworm moving around a roof, shingle spawn action works

--- inchworm_control/scripts/action_client_example.py
+++ inchworm_control/scripts/action_client_example.py
@@ -5,23 +5,23 @@ import rospy, actionlib
 from inchworm_control.msg import InchwormAction, InchwormGoal
 
 def spawnShingles(spawn, client_0):
-  # spawn.action_type = 3
-  # spawn.coord_x = 0
-  # spawn.coord_y = 1
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
-  # spawn.coord_x = 1
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
-  # spawn.coord_x = 2
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
-  # spawn.coord_x = 3
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
-  # spawn.coord_x = 4
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
+  spawn.action_type = 3
+  spawn.coord_x = 0
+  spawn.coord_y = 1
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 1
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 2
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 3
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 4
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
 
 
   spawn.coord_x = 0
@@ -41,22 +41,22 @@ def spawnShingles(spawn, client_0):
   client_0.send_goal(spawn)
   client_0.wait_for_result()
 
-  #spawn.coord_x = 0
-  #spawn.coord_y = 3
-  #client_0.send_goal(spawn)
-  #client_0.wait_for_result()
-  #spawn.coord_x = 1
-  #client_0.send_goal(spawn)
-  #client_0.wait_for_result()
-  #spawn.coord_x = 2
-  #client_0.send_goal(spawn)
-  #client_0.wait_for_result()
-  # spawn.coord_x = 3
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
-  # spawn.coord_x = 4
-  # client_0.send_goal(spawn)
-  # client_0.wait_for_result()
+  spawn.coord_x = 0
+  spawn.coord_y = 3
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 1
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 2
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 3
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
+  spawn.coord_x = 4
+  client_0.send_goal(spawn)
+  client_0.wait_for_result()
 
 
 def walk0(goal0, x, y, ee):
@@ -81,8 +81,8 @@ def main():
   client_0 = actionlib.SimpleActionClient("/inchworm_action_0", InchwormAction)
   client_0.wait_for_server()
 
-  client_1 = actionlib.SimpleActionClient("/inchworm_action_1", InchwormAction)
-  client_1.wait_for_server()
+  # client_1 = actionlib.SimpleActionClient("/inchworm_action_1", InchwormAction)
+  # client_1.wait_for_server()
 
   rospy.loginfo("Got servers, sending goals.")
 
@@ -92,21 +92,30 @@ def main():
   goal1 = InchwormGoal()
   spawn = InchwormGoal()
 
-  #spawnShingles(spawn, client_0)
-
-  walk0(goal0, 1, 1, 1)
-  walk1(goal1, 1, 0, 1)
+  rospy.loginfo("Spawning shingles 5x5 roof start")
+  spawnShingles(spawn, client_0)
+  rospy.loginfo("Shingles spawned")
+  walk0(goal0, 0, 1, 1)
+  # walk1(goal1, 1, 0, 1)
   client_0.send_goal(goal0)
-  client_1.send_goal(goal1)
+  # client_1.send_goal(goal1)
   client_0.wait_for_result()
-  client_1.wait_for_result()
+  # client_1.wait_for_result()
 
-  walk0(goal0, 2, 1, 0)
-  walk1(goal1, 0, 0, 0)
+  walk0(goal0, 1, 1, 0)
   client_0.send_goal(goal0)
-  client_1.send_goal(goal1)
   client_0.wait_for_result()
-  client_1.wait_for_result()
+
+  walk0(goal0, 1, 0, 1)
+  client_0.send_goal(goal0)
+  client_0.wait_for_result()
+
+  # walk0(goal0, 2, 1, 0)
+  # walk1(goal1, 0, 0, 0)
+  # client_0.send_goal(goal0)
+  # client_1.send_goal(goal1)
+  # client_0.wait_for_result()
+  # client_1.wait_for_result()
 
 
   # walk0(goal0, 0, 2, 0)
