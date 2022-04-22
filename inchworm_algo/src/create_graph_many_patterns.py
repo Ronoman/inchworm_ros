db2db8e15d45b531fe2ae544f285c2b5ca7a6e78
Filip Kernan <filip.kernan@gmail.com>
2022-04-21 22:45:22 -0400

    created beter time graph

---
+++ inchworm_algo/src/create_graph_many_patterns.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from cProfile import label
+import csv, sys, pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+move_to_days = (16/3600)
+WIDTH = 30
+HEIGHT = 30
+pattern = 0
+
+data = []
+for i in range(3):
+    with open(f"../data/{sys.argv[1]}") as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+        for row in reader:
+            # print(row)
+            row = row[0].split(',')
+            row = [int(x) for x in row]
+            row_data = {}
+            row_data["inchworm_count"] = row[0]
+            row_data["ticks"] = row[1]
+            row_data["move_counts"] = row[2: 2+ row_data["inchworm_count"]]
+            row_data["state_data"] = {
+                    "inchworm_id": [],
+                    "Pickup From Depot": [],
+                    "Move to Target": [], 
+                    "Install Shingle": [],
+                    "Move Shingle": [],
+                    "Explore": [],
+                    "Move to Depot": []
+                }
+            for i in range(row_data["inchworm_count"]):
+                data_index = 2 + row_data["inchworm_count"] + i * 6
+                row_sum = 0
+                for i in range(6):
+                    row_sum += row[data_index + i]
+                row_data["state_data"]['inchworm_id'].append(i)
+                row_data["state_data"]['Pickup From Depot'].append(100 * row[data_index]/row_sum)
+                row_data["state_data"]['Move to Target'].append(100 * row[data_index + 1]/row_sum)
+                row_data["state_data"]['Install Shingle'].append(100 * row[data_index + 2]/row_sum)
+                row_data["state_data"]['Move Shingle'].append(100 * row[data_index + 3]/row_sum)
+                row_data["state_data"]['Explore'].append(100 * row[data_index + 4]/row_sum)
+                row_data["state_data"]['Move to Depot'].append(100 * row[data_index + 5]/row_sum)
+
+                # print(row_data["state_data"])
+            data.append(row_data)
+    inchworms = []
+    move_data = []
+    average_data = pd.DataFrame()
+    
+    plt.rcParams['font.size'] = '14'
+    plt.scatter(inchworms, move_data, label=f"Pattern {i}")
+plt.xlabel("Inchworm Count")
+plt.ylabel("Estimated Hours Until Compilation")
+plt.title(f"Time to Shingle a {WIDTH}x{HEIGHT} Roof")
+
+plt.savefig(f"{WIDTH}x{HEIGHT}_{pattern}.png")
+
+
+
+plt.savefig(f"{WIDTH}x{HEIGHT}_time.png")
+plt.show()
